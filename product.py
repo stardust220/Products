30a622328a9a4d30d4b5beae13e2efe2d3f02510
stardust220 <r01941003@gmail.com>
2019-08-04 21:05:17 +0900

    add sum function

--- product.py
+++ product.py
@@ -1,19 +1,17 @@
 #加入是否檔案在不在
 import os # operating system
 products = []
-def read_file(filename):#讀取檔案 #檔名建議寫在parameter
-    if os.path.isfile(filename):
-        print('yes, the file exists')
-        with open(filename,'r',encoding ='utf-8') as f:
-            for line in f:
-                if '商品,價格' in line:
-                    continue
-                name, price = line.strip().split(',')  #Split切割後的結果是清單
-                products.append([name,price])
+#讀取檔案 #檔名建議寫在parameter
+def read_file(filename):
+    with open(filename,'r',encoding ='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')  #Split切割後的結果是清單
+            products.append([name,price])
         print (products)
-    else:
-        print('cannot find the file')
-    return products
+        return products
+
 #讓使用者輸入
 def user_input(products):
     while True:
@@ -30,15 +28,29 @@ def print_products(products):
     for p in products:
         print(p[0], 'price is', p[1])
 
-def  write_file(filename,products):
+def write_file(filename,products): 
     with open(filename, 'w', encoding ='utf-8') as f: #寫入模式,並且明確告知我要用utf編碼寫入
         f.write('商品,價格\n') #加上第一行的欄位
         for p in products:
             f.write(p[0] + ',' + str(p[1]) + '\n') #轉換價錢為文字型態才能與字串合併
 
+def total_cost(products,sum = 0):
+    for p in products:
+        sum = sum + int(p[1])
+    print('The total cost is', sum)
+    return sum
 
-products = read_file('products.csv')
-products = user_input(products)
-print_products(products)
-write_file('products.csv',products)
+#主要執行程式碼 main function
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('yes, the file exists')
+        products = read_file(filename)
+    else:
+        print('no file in the folder')
 
+    products = user_input(products)
+    print_products(products)
+    write_file(filename,products)
+    total_cost(products)
+main()
